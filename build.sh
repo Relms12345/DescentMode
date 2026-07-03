024e3250e04d195e611316510aba1de009c123d9
relms <relms@relms.dev>
2026-07-03 02:41:02 -0700

    update

--- build.sh
+++ build.sh
@@ -3,17 +3,24 @@ set -e
 
 cd "$(dirname "$0")"
 
-# Include your custom env variables
 [ -e ./.env ] && . .env
 
 rm -rvf bin obj packages
 
-export XBUILD=${XBUILD:-/usr/bin/msbuild}
-export KSPPATH=${KSPPATH:-~/games/KSP-GOG/1.4.3/game}
+export KSP_ROOT=${KSP_ROOT:-~/games/KSP-GOG/1.4.3/game}
 
-${XBUILD} /p:Configuration=Release DescentMode.sln
-mkdir -pv GameData/DescentMode/Plugins
-cp -av bin/Release/*.dll GameData/DescentMode/Plugins
+# Detect managed path (KSP 1.8+ uses KSP_x64_Data, older uses KSP_Data)
+if [ -d "$KSP_ROOT/KSP_x64_Data/Managed" ]; then
+    KSPBT_MANAGED="$KSP_ROOT/KSP_x64_Data/Managed"
+elif [ -d "$KSP_ROOT/KSP_Data/Managed" ]; then
+    KSPBT_MANAGED="$KSP_ROOT/KSP_Data/Managed"
+else
+    echo "ERROR: KSP managed DLLs not found at $KSP_ROOT"
+    echo "Set KSP_ROOT to a valid KSP install directory"
+    exit 1
+fi
+
+dotnet build -c Release -p:KSPBT_ManagedPath="$KSPBT_MANAGED"
 
 mkdir -pv packages
 zip -r -v packages/DescentMode.zip GameData LICENSE.txt README.md
